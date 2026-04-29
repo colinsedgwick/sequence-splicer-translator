7fd7512785b345e5e3ae632352939f9a7adf0626
Colin Sedgwick <colinsedgwick@utexas.edu>
2026-04-29 16:44:09 +0000

    updated app

--- app.py
+++ app.py
@@ -76,10 +76,9 @@ app.layout = dbc.Container([
                     "wordBreak": "break-word"
                 }
             ),
-            #AI Usage: 
+            
             dbc.Button("Download mRNA FASTA file", id='btn-download-mrna'),
-            dcc.Download(id='download-mrna'),
-            dcc.Store(id='mrna-store')
+            dcc.Download(id='download-mrna'),  
         ]),
     ]),
     dbc.Row([
@@ -93,15 +92,40 @@ app.layout = dbc.Container([
                     "wordBreak": "break-word"
                 }
             ),
-            #AI Usage: 
+             
             dbc.Button("Download protein FASTA file", id='btn-download-protein'),
             dcc.Download(id='download-protein'),
-            dcc.Store(id='protein-store')
+            
         ])
     ]),
 
 ], fluid=True)
 
+# callback function that takes generated mRNA sequence and downloads it as FASTA upon download button being clicked
+@callback(
+    Output('download-mrna', 'data'),
+    Input('btn-download-mrna', 'n_clicks'),
+    State('mrna-output', 'children'),
+    prevent_initial_call=True
+)
+def download_mrna(click, mrna_sequence):
+
+    return dict(content=(f">mrna_sequence\n{mrna_sequence}"), filename='output_mrna.fasta')
+
+# callback funciton that takes generated protein sequence and downloads it as FASTA upon download button being clicked
+@callback(
+    Output('download-protein', 'data'),
+    Input('btn-download-protein', 'n_clicks'),
+    State('protein-output', 'children'),
+    prevent_initial_call=True
+)
+def download_protein(click, protein_sequence):
+
+    return dict(content=(f">protein_sequence\n{protein_sequence}"), filename='output_protein.fasta')
+
+
+# callback function that takes sequence and coordinates inputs to process and return output sequences when 
+# process button is clicked
 @callback(
     [Output('protein-output', 'children'),
     Output('mrna-output', 'children'),
@@ -116,6 +140,7 @@ app.layout = dbc.Container([
 )
 def process_sequence(process_clicks, reset_clicks, fasta_contents, dna_sequence, exon_string, cds_string):
     
+    # if reset button is clicked, resets all previously output sequences
     if ctx.triggered_id == 'reset-button':
         return (
             None,
@@ -123,12 +148,20 @@ def process_sequence(process_clicks, reset_clicks, fasta_contents, dna_sequence,
             None
         )
     
+    # check if inputs are present
     if (dna_sequence and exon_string and cds_string):
         dna_sequence = dna_sequence.upper()
         input = "text"
     elif (fasta_contents and exon_string and cds_string):
-        dna_sequence = parse_uploaded_fasta(fasta_contents)
-        input = "uploaded file"
+        try:
+            dna_sequence = parse_uploaded_fasta(fasta_contents)
+            input = "uploaded file"
+        except Exception:
+            return (
+            "",
+            "",
+            dbc.Alert("Please upload valid FASTA with single DNA sequence.", color="warning")
+        )   
     else:
         return (
             "",
@@ -138,10 +171,9 @@ def process_sequence(process_clicks, reset_clicks, fasta_contents, dna_sequence,
     
 
     try:
-        
+        # process sequence and coordinates with sequence_processor module
         exon_coordinates = seqpr.coords_string_to_tuple(exon_string)
         mature_mrna = seqpr.mature_mrna_generator(dna_sequence, exon_coordinates)
-
         cds_positions = seqpr.cds_pos_to_tuple(cds_string)
         protein_sequence = seqpr.mrna_translator(mature_mrna, cds_positions)
 
@@ -152,6 +184,7 @@ def process_sequence(process_clicks, reset_clicks, fasta_contents, dna_sequence,
                 ])
         )
 
+    # uses SystemExit as exception to handle because sequence_processor module handles invalid inputs with sys.exit(1)
     except SystemExit:
         return (
             None,
@@ -160,24 +193,46 @@ def process_sequence(process_clicks, reset_clicks, fasta_contents, dna_sequence,
 
         )
 
+    # handles all other exceptions not handled in sequence_processor module
     except Exception as e:
         return (
             None,
             None,
             f"Error: {e}"
             )
+
+
+def parse_uploaded_fasta(contents: str) -> str:
+    """
+    Given input FASTA file contents from uploaded file, converts to Python string, processes, and returns sequence.
+
+    Args:
+        contents: FASTA file contents from uploaded file as a string
+
+    Returns:
+        returns the sequence from the FASTA file as a string
+    """
     
-def parse_uploaded_fasta(contents):
+    #split the content information from the actual content and convert to Python string
     content_type, content_string = contents.split(',')
     decoded = base64.b64decode(content_string).decode('utf-8')
 
+    # iterates through decoded contents line by line to check for unique sequences and add sequence lines to a list
+    sequence_counter = 0
     sequence_lines = []
     for line in decoded.splitlines():
-        if line.strip() and not line.startswith(">"):
+        if line.startswith(">"):
+            sequence_counter += 1
+        elif line.strip() and not line.startswith(">"):
             sequence_lines.append(line.strip())
 
-
+    # if there is more or less than one sequnence in the file, raises exception
+    if sequence_counter != 1:
+        raise Exception
+    
+    # join the sequence fragments from the list into one string that is returned
     return "".join(sequence_lines).upper()
+    
 
 
 
